0f38f87a826877db9116ffb7f980127653c4437c
ErickVO <59853887+ErickVO@users.noreply.github.com>
2020-05-22 23:47:00 -0400

    Add project files.

---
+++ NuevoProyecto/BLL/RegistroBLL.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NuevoProyecto.DAL;
+using NuevoProyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NuevoProyecto.BLL
+{
+    public class RegistroBLL
+    {
+        public static bool Guardar(Datos datos)
+        {
+            if (!Existe(datos.Id))
+            {
+                return Insertar(datos);
+            }
+            else 
+            {
+                return Modificar(datos);
+            }
+
+           
+        } 
+
+
+        private static bool Insertar(Datos datos)
+        {
+            Contexto db = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                db.Datos.Add(datos);
+                paso = (db.SaveChanges() > 0);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+
+            return paso;
+        }
+
+        private static bool Modificar (Datos datos)
+        {
+            Contexto db = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                db.Entry(datos).State = EntityState.Modified;
+                paso = (db.SaveChanges() > 0);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return paso;
+        
+        }
+
+        public static bool Eliminar(int id)
+        {
+            Contexto db = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                var datos = db.Datos.Find(id);
+
+                if(datos != null)
+                {
+                    db.Datos.Remove(datos);
+                    paso = (db.SaveChanges() > 0);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose(); 
+            }
+
+            return paso;
+        }
+
+
+        public static bool Existe(int id)
+        {
+            Contexto db = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = db.Datos.Any(d => d.Id == id);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                db.Dispose();
+            }
+            return encontrado;
+        }
+
+
+
+
+    }
+}
---
+++ NuevoProyecto/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NuevoProyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NuevoProyecto.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Datos> Datos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source = Registro.db");
+        }
+    }
+}
